ba2c043046c600a9b811861663a4f28b0b3eb287
AnimeshN <animesh.nauti@gmail.com>
2021-01-08 23:26:08 +0530

    error removed

--- predictor/views.py
+++ predictor/views.py
@@ -10,7 +10,7 @@ class descisionTree(APIView):
     def get(self,*args,**kwargs):
             #reading values form url
             val1 = self.kwargs.get('Geomorphology', None)
-            val2 = self.kwargs.get('RochChar', None)
+            val2 = self.kwargs.get('RockChar', None)
             val3 = self.kwargs.get('OverburdenThickness', None)
             val4 = self.kwargs.get('Hydrology', None)
             val5 = self.kwargs.get('Erosion', None)
@@ -19,25 +19,26 @@ class descisionTree(APIView):
             val8 = self.kwargs.get('SlopeType', None)
             val9 = self.kwargs.get('LandslideMaterial', None)
             val10 = self.kwargs.get('Movement', None)
-            val10 = self.kwargs.get('Style', None)
+            val11 = self.kwargs.get('Style', None)
 
+    
             #converting labels to integers
 
 
             Geomorphology = int(PredictorConfig.le_Geomorphology.transform([val1])[0])
-            RochChar = int(PredictorConfig.le_RockChar.transform([val2])[0])
+            RockChar = int(PredictorConfig.le_RockChar.transform([val2])[0])
             OverburdenThickness = int(PredictorConfig.le_OverburdenThickness.transform([val3])[0])
-            Hydrology = int(PredictorConfig.le_RochChar.transform([val4])[0])
+            Hydrology = int(PredictorConfig.le_Hydrology.transform([val4])[0])
             Erosion = int(PredictorConfig.le_Erosion.transform([val5])[0])
             Rainfall = int(PredictorConfig.le_Rainfall.transform([val6])[0])
             Anthropogenic = int(PredictorConfig.le_Anthropogenic.transform([val7])[0])
             SlopeType = int(PredictorConfig.le_SlopeType.transform([val8])[0])
             LandslideMaterial = int(PredictorConfig.le_LandslideMaterial.transform([val9])[0])
             Movement = int(PredictorConfig.le_Movement.transform([val10])[0])
-            Style = int(PredictorConfig.le_Style.transform([val10])[0])
+            Style = int(PredictorConfig.le_Style.transform([val11])[0])
             
             #predicting value
-            result = PredictorConfig.model.predict([[Geomorphology,RochChar,OverburdenThickness,Hydrology,Erosion,Rainfall,Anthropogenic,SlopeType,LandslideMaterial,Movement,Style]])
+            result = PredictorConfig.model.predict([[Geomorphology,RockChar,OverburdenThickness,Hydrology,Erosion,Rainfall,Anthropogenic,SlopeType,LandslideMaterial,Movement,Style]])
             
             #inversing the integer to value
             susceptibility = PredictorConfig.le_target.inverse_transform(result)[0]
